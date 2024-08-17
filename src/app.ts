5f9bd7876e200d8eb4f94ec3e90ce8d79b3a8bbb
shahin2525 <shahin451999@gmail.com>
2024-08-16 18:16:44 -0700

    add user interface

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,10 @@ import express from 'express'
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.status(200).json({
+    status: 'success',
+    message: 'welcome to murir tin',
+  })
 })
 
 export default app
---
+++ src/interfaces/user.interface.ts
@@ -0,0 +1,9 @@
+interface IUser {
+  name: string
+  age: number
+  email: string
+  photo: string
+  role: 'user' | 'admin'
+  userStatus: 'active' | 'inactive'
+}
+export { IUser }
