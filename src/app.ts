bb5f7e99ff1a2df17db312835815e3b002bf3538
shahin2525 <shahin451999@gmail.com>
2024-08-16 23:41:56 -0700

    add services

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
 import express from 'express'
-import cors from 'cors'
+// import cors from 'cors'
 import { UserRouter } from './routes/user.route'
 const app = express()
 
 app.use(express.json())
-app.use(cors())
+// app.use(cors())
 
 app.use('/api/v1/users/', UserRouter)
 
