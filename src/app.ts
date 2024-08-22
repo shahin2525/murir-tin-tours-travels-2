8f36ac8f0294a5ff5070eb4a6b346fae5ebcbc1d
shahin2525 <shahin451999@gmail.com>
2024-08-21 20:45:03 -0700

    add review create api

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import { UserRouter } from './routes/user.route'
 import { TourRouter } from './routes/tour.route'
+import { ReviewRouter } from './routes/review.route'
 const app = express()
 
 app.use(express.json())
@@ -9,6 +10,7 @@ app.use(cors())
 
 app.use('/api/v1/users/', UserRouter)
 app.use('/api/v1/tours/', TourRouter)
+app.use('/api/v1/reviews', ReviewRouter)
 
 app.get('/', (req, res) => {
   res.status(200).json({
--- src/models/review.model.ts
+++ src/models/review.model.ts
@@ -1,33 +1,41 @@
 import { model, Schema } from 'mongoose'
 import { IReview } from '../interfaces/review.interface'
 
-const reviewSchema = new Schema<IReview>({
-  review: {
-    type: String,
-    required: [true, 'A review must have text'],
-    trim: true,
+const reviewSchema = new Schema<IReview>(
+  {
+    review: {
+      type: String,
+      required: [true, 'A review must have text'],
+      trim: true,
+    },
+    rating: {
+      type: Number,
+      required: [true, 'A review must have a rating'],
+      min: [1, 'Rating must be above 1.0'],
+      max: [5, 'Rating must be below 5.0'],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    tour: {
+      type: Schema.Types.ObjectId,
+      ref: 'Tour',
+      required: [true, 'Review must belong to a tour'],
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: [true, 'Review must belong to a user'],
+    },
   },
-  rating: {
-    type: Number,
-    required: [true, 'A review must have a rating'],
-    min: [1, 'Rating must be above 1.0'],
-    max: [5, 'Rating must be below 5.0'],
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
   },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  tour: {
-    type: Schema.Types.ObjectId,
-    ref: 'Tour',
-    required: [true, 'Review must belong to a tour'],
-  },
-  user: {
-    type: Schema.Types.ObjectId,
-    ref: 'User',
-    required: [true, 'Review must belong to a user'],
-  },
-})
+)
+
+reviewSchema.index({ user: 1, tour: 1 }, { unique: true })
 
 const Review = model<IReview>('Review', reviewSchema)
 
